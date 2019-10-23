7d47bbddde5e1ccf57744219db6918565174b6c7
gerinsp <denapratama7@gmail.com>
2019-10-23 22:54:22 +0700

    OOP-PHP

---
+++ autoloading/index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neli Drukmann", "Sony Computer", 250000, 50);
+
+$cetakProduk = new cetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
+
+?>
---
+++ constant.php
@@ -0,0 +1,20 @@
+<?php 
+
+// cara penulisan konstanta
+// define('NAMA', 'Gerinsp');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 19;
+// echo UMUR;
+
+
+
+class Coba {
+    const NAMA = 'Gerinsp';
+}
+
+echo Coba::NAMA;
+
+?>
